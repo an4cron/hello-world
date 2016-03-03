cb4678f8783a60bc2404856cb0d1f256d3d22dab
anacron <coccios@hotmail.com>
2016-03-03 11:15:37 +0100

    Aggiunta print a video

--- hello-world/Program.cs
+++ hello-world/Program.cs
@@ -8,6 +8,10 @@ namespace hello_world
 {
     class Program
     {
+        /// <summary>
+        /// Aggiunta print a video
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine("HEllo World : Peppino");
